e49b01e289bcd68552cc8b2171766eb6347020c9
AminSalkovic <aminsalkovic2019@gmail.com>
2023-03-13 20:35:47 +0100

    snake-game

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 using namespace std;
 
+bool gameOver;
+const int width=20;
+const int height=20;
+int x,y,fruitX,fruitY,score;
+enum eDirection {STOP=0,LEFT,RIGHT,UP,DOWN};
+eDirection dir;
+
+void Setup(){
+  gameOver=false;
+  dir=STOP;
+  x=width / 2;
+  y=height / 2;
+  fruitX = rand() % width;
+  fruitY = rand() % height;
+  score=0;
+}
+void Draw(){
+  system("cls")
+}
+void Input(){
+
+}
+void Logic(){
+
+}
+
 int main(){
-    cout << "amins" << endl;
-    return 0;
+   Setup();
+   while (!gameOver){
+    Draw();
+    Input();
+    Logic();
+   }
 }
