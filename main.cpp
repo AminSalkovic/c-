0ead312b55517290f1cb1ac521c38b8b8228ebc7
AminSalkovic <aminsalkovic2019@gmail.com>
2023-03-15 15:53:57 +0100

    snake

--- main.cpp
+++ main.cpp
@@ -2,36 +2,25 @@
 using namespace std;
 
 bool gameOver;
-const int width=20;
-const int height=20;
-int x,y,fruitX,fruitY,score;
-enum eDirection {STOP=0,LEFT,RIGHT,UP,DOWN};
-eDirection dir;
+const int width = 20;
+const int height = 10;
+int x, y, fruitX, fruitY, score;
 
-void Setup(){
-  gameOver=false;
-  dir=STOP;
-  x=width / 2;
-  y=height / 2;
-  fruitX = rand() % width;
-  fruitY = rand() % height;
-  score=0;
-}
 void Draw(){
-  system("cls")
-}
-void Input(){
-
-}
-void Logic(){
-
+  system("cls");
+  for(int i=0;i<width+2;i++){
+    cout<<"#";
+  }
+  for(int i=0;i<height;i++){
+    cout <<"#"<<endl;
+  }
+  cout << endl;
+  for(int i=0;i<width+2;i++){
+    cout<<"#";
+  }
 }
 
 int main(){
-   Setup();
-   while (!gameOver){
     Draw();
-    Input();
-    Logic();
-   }
+    return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,22 @@
-and()
+for(int i=0;i < width+2;i++){
+  //   cout << "#"<<endl;
+  // }
+   
+  //  for(int i=0;i < height;i++){
+  //      for(int j=0;i< width;i++){
+  //          if(j==0)
+  //            cout <<"#";
+  //          else 
+  //           cout << " ";
+         
+  //        if(j== width-1){
+  //           cout << "#";
+  //        }
+  //      }
+  //      cout << endl;
+  //  }
+   
+  //  for(int i=0;i < width+2;i++){
+  //   cout << "#"<<endl;
+    
+  // }
